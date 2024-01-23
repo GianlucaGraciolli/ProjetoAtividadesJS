46a42d92d2d838dd044bb3d18e8e7806f0c17d61
Gianluca <gianluca.graciolli@hotmail.com>
2024-01-22 22:25:55 -0300

    DESENVOLVENDO ATIVIDADE JS IMC

---
+++ AtividadeIMC/javaScript/script.js
@@ -0,0 +1,29 @@
+function calcularIMC(){
+let nomeusuario = document.querySelector('.input-nome').value;
+let pesousuario = document.querySelector('.input-peso').value;
+let alturausuario = document.querySelector('input-altura').value;
+
+let resultadoIMC = pesousuario * alturausuario^2;
+let grau, nivel;
+
+if (resultadoIMC < 18.5) {
+    grau = 0;
+    nivel = 'Magreza';
+} else if (resultadoIMC < 25) {
+    grau = 0;
+    nivel = 'Normal';
+} else if (resultadoIMC < 30) {
+    grau = 1;
+    nivel = 'Sobrepeso';
+} else if (resultadoIMC <40) {
+    grau = 2;
+    nivel = 'Obesidade';
+} else {
+    grau = 3;
+    nivel = 'Obesidade Grave';
+}
+
+let mensagem = `${nomeusuario}, seu IMC é ${resultadoIMC}! Resultado: ${nivel} Grau de Obesidade: ${grau}`;
+
+document.querySelector('.resultado').innerHTML = mensagem;
+}
